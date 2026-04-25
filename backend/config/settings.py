1522e84ec3ee46397d9dfd51f9cf75859ff23421
Turaboy Holmirzaev <turabekh@gmail.com>
2026-04-25 12:17:06 +0800

    Phase 0: production Dockerfiles and docker-compose.prod.yml

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -84,7 +84,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
